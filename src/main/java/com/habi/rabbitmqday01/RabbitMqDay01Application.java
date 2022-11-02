4a3dc8179e6d6b2a89f0fef07c868959806df361
allubiba-H <spider_h@qq.com>
2022-11-02 14:07:04 +0800

    RabbitMq

---
+++ src/main/java/com/habi/rabbitmqday01/RabbitMqDay01Application.java
@@ -0,0 +1,13 @@
+package com.habi.rabbitmqday01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqDay01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqDay01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/habi/rabbitmqday01/RabbitMqDay01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habi.rabbitmqday01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqDay01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
